6bb9bf212a5c22f67bb86835f3c1f3f9c92848bd
unknown <Willian Castro>
2023-01-05 15:58:39 -0500

    Primera modificacion

--- factorial-break.js
+++ factorial-break.js
@@ -4,8 +4,8 @@ i = 1
 while(i => 1){
   factorial *= i
   i++
-  if(i == 10){
+  if(i == 12){
     break
   }
 }
-console.log("El tactorial de 10 es : " +factorial)
+console.log("El tactorial de 12 es : " +factorial)
