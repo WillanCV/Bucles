646f396a74fa2222f0c54cfd09251d535b0d05eb
unknown <Willian Castro>
2023-01-05 15:10:02 -0500

    Mis bucles

---
+++ factorial-break.js
@@ -0,0 +1,11 @@
+let factorial = 1
+i = 1
+
+while(i => 1){
+  factorial *= i
+  i++
+  if(i == 10){
+    break
+  }
+}
+console.log("El tactorial de 10 es : " +factorial)
---
+++ factorial-for.js
@@ -0,0 +1,6 @@
+let numero = 10
+let factorial = 1
+for(let i = 1; i <= numero; i++){
+  factorial *= i
+}
+console.log("El factorial de 10 es : " +factorial)
