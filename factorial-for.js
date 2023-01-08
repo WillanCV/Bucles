26089f92974e019245402d408f361daeceaafbe0
Willian Castro <wilfredocastro1@hotmail.com>
2023-01-08 18:49:25 -0500

    numero7

--- factorial-for.js
+++ factorial-for.js
@@ -1,6 +1,6 @@
-let numero = 12
+let numero = 7
 let factorial = 1
 for(let i = 1; i <= numero; i++){
   factorial *= i
 }
-console.log("El factorial de 12 es : " +factorial)
+console.log("El factorial de numero 7 es : " +factorial)
