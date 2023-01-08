c6ba1e35274803d268a3581b7e7f04908d9a1267
Willian Castro <wilfredocastro1@hotmail.com>
2023-01-08 18:31:21 -0500

    el propio

--- factorial-while.js
+++ factorial-while.js
@@ -1,7 +1,7 @@
 let factorial = 1
 i = 1
-while(i <= 10){
+while(i <= 12){
   factorial *= i
   i++
 }
-console.log("El factorial de10 es : " +factorial)
+console.log("El factorial de12 es : " +factorial)
